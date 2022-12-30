5eb1402077581b88cc1a3a59a0670e8a668f7117
pasly0920 <inwoo920621@gmail.com>
2022-12-30 11:37:35 +0900

    feat: member model 추가

---
+++ src/main/java/hellojpa/Member.java
@@ -0,0 +1,13 @@
+package hellojpa;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Member {
+    @Id
+    private Long id;
+    private String name;
+}
