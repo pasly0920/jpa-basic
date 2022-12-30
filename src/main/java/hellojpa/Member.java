0413d5a75d1657ad9affae941c070e2bd28111f6
pasly0920 <inwoo920621@gmail.com>
2022-12-30 18:28:46 +0900

    feat: constructor 추가

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,13 +1,24 @@
 package hellojpa;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Getter @Setter
+@Getter
+@Setter
 public class Member {
+
     @Id
     private Long id;
     private String name;
+
+    public Member() {
+    }
+
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
