0aca1d49906093735b7b481cc24c6c56b6e455cc
pasly0920 <inwoo920621@gmail.com>
2023-01-04 14:22:06 +0900

    feat: jpa 상속 관계 매핑 전략

---
+++ src/main/java/hellojpa/Item.java
@@ -0,0 +1,21 @@
+package hellojpa;
+
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn
+public abstract class Item {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+    private int price;
+}
