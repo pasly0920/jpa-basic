1bacb9d1ad031dd6e7bad87bae12c1f6c2469397
pasly0920 <inwoo920621@gmail.com>
2023-01-07 08:37:06 +0900

    feat: 값 타입 비교 메서드 정의

--- src/main/java/hellojpa/Address.java
+++ src/main/java/hellojpa/Address.java
@@ -1,5 +1,6 @@
 package hellojpa;
 
+import java.util.Objects;
 import javax.persistence.Embeddable;
 import lombok.Getter;
 
@@ -20,4 +21,22 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Address address = (Address) o;
+        return Objects.equals(city, address.city) && Objects.equals(street,
+                address.street) && Objects.equals(zipcode, address.zipcode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, street, zipcode);
+    }
 }
